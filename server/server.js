fec13bd136233b4119634c3b7e181d29c7accfc9
sam80719 <sam80719@gmail.com>
2018-04-25 22:16:08 +0800

    add GET todo route

--- server/server.js
+++ server/server.js
@@ -6,9 +6,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
-
 var { mongoose } = require('./db/mongoose');
-
 var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
 
@@ -25,18 +23,26 @@ app.post('/todos', (req, res) => {
 
     todo.save().then((doc) => {
     	res.send(doc);
-    }, (err) => {
-    	res.status(400).send(err);
+    }, (e) => {
+    	res.status(400).send(e);
     });
 });
 
-
+app.get('/todos', (req,res)=>{
+    Todo.find().then((todos)=>{
+        res.send({todos});
+    }, (e)=>{
+        res.status(400).send(e);
+    })
+});
 
 
 
 app.listen(3000, () => {
     console.log('started on port 3000');
 });
+
+module.exports = {app};
 
 
 // var newTodo = new Todo({
