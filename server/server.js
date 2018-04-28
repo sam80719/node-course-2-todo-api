1d5b7bc717d98e14a24711c180b79439ac56b69a
sam80719 <sam80719@gmail.com>
2018-04-28 10:09:41 +0800

    setup app for heroku

--- server/server.js
+++ server/server.js
@@ -12,6 +12,7 @@ var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
 
 var app = express();
+const port = process.env.PORT||3000;
 
 
 app.use(bodyParser.json());
@@ -52,7 +53,7 @@ app.get('/todos/:id', (req, res) => {
       //if no todo- send back 404 with empty body
      //error
       //400 - and send empty body back
-     
+
     if(!ObjectID.isValid(id)) {
         return res.status(404).send();
     }
@@ -77,8 +78,8 @@ app.get('/todos/:id', (req, res) => {
 });
 
 
-app.listen(3000, () => {
-    console.log('started on port 3000');
+app.listen(port, () => {
+    console.log(`started up at port ${port}`);
 });
 
 module.exports = { app };
