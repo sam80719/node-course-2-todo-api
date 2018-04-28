746e544cf13e2d8e2a69cd86a35ae7c59ce15285
sam80719 <sam80719@gmail.com>
2018-04-28 09:04:12 +0800

    add test case for GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -45,6 +45,14 @@ app.get('/todos/:id', (req, res) => {
 
     //valid id using is valid
      // 404-send back empty send
+
+    //findById
+     // success
+      //if todo -send it back
+      //if no todo- send back 404 with empty body
+     //error
+      //400 - and send empty body back
+     
     if(!ObjectID.isValid(id)) {
         return res.status(404).send();
     }
