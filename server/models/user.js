c2fb88b3669318277bf8226c09d869c9237c0973
sam80719 <sam80719@gmail.com>
2018-05-03 21:47:02 +0800

    add post /users route and token method

--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
 var UserSchema = new mongoose.Schema({
     email: {
@@ -35,18 +36,30 @@ var UserSchema = new mongoose.Schema({
     }]
 });
 
-UserSchema.methods.generateAuthToken = function() {
-    var user = this;
-    var access = 'auth';
-    var token = jwt.sign({ _id: user._id.toHexString(), access }, 'abc123').toString();
-    user.tokens.push({ access, token }); //version mongo 2.0
-    // user.tokens = user.tokens.concat([{access, token}]); //version mongo 3.0
-    return user.save().then(() => {
-        return token;
 
-    });
 
+UserSchema.methods.toJSON = function () {
+  var user = this;
+  var userObject = user.toObject();
+
+  return _.pick(userObject, ['_id', 'email']);
 };
+
+
+
+UserSchema.methods.generateAuthToken = function () {
+  var user = this;
+  var access = 'auth';
+  var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+
+  // user.tokens.push({access, token});
+  user.token = user.tokens.concat([{access, token}]);
+
+  return user.save().then(() => {
+    return token;
+  });
+};
+
 
 var User = mongoose.model('User', UserSchema);
 module.exports = { User };
--- server/server.js
+++ server/server.js
@@ -145,25 +145,36 @@ app.patch('/todos/:id', (req, res) => {
 });
 
 //POST/user
-app.post('/users', (req,res)=>{
-    var body = _.pick(req.body, ['email','password']);
-    var user = new User(body);
+// app.post('/users', (req,res)=>{
+//     var body = _.pick(req.body, ['email','password']);
+//     var user = new User(body);
 
 
-    // User.findByToken
-    // User.generateAuthToken
+//     // User.findByToken
+//     // User.generateAuthToken
 
-    user.save().then(()=>{
-        return user.generateAuthToken();
-        // res.send(user);
-    }).then((token)=>{
-        res.header('x-auth', token).send(user);
-    }).catch((e)=>{
-        res.status(400).send(e);
-    })
+//     user.save().then(()=>{
+//         return user.generateAuthToken();
+//         // res.send(user);
+//     }).then((token)=>{
+//         res.header('x-auth', token).send(user);
+//     }).catch((e)=>{
+//         res.status(400).send(e);
+//     })
+// });
+
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  })
 });
-
-
 
 
 
